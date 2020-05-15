83dedfb2c63961a256b32731b00aba1567d77b3d
Josh Metry <Josh.Metry@fresno.edu>
2020-05-15 16:19:19 -0700

    added new users access as well as upgraded attendance entrance to allow multiples on one line

--- Attendance.aspx.vb
+++ Attendance.aspx.vb
@@ -6,15 +6,27 @@ Partial Class Attendance
         If IsNothing(Session.Item("Name")) = True Then
             Response.Redirect("default.aspx")
         Else
-            lblEventName.Text = Session.Item("Name").ToString
+            If IsPostBack = False Then
+                lblEventName.Text = Session.Item("Name").ToString
+            End If
         End If
     End Sub
     Protected Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
 
         SqlDataSource1.InsertParameters.Add("UploadedBy", Request.ServerVariables("LOGON_USER"))
+        Me.TextBox1.Text = Me.TextBox1.Text.Replace(" ", "")
+        Me.TextBox1.Text = Me.TextBox1.Text.Replace(",", vbLf)
+
         For Each line As String In Me.TextBox1.Text.Split(vbLf)
-            SqlDataSource1.InsertParameters("List").DefaultValue = line.ToString
-            SqlDataSource1.Insert()
+            If line <> "" Then
+                SqlDataSource1.InsertParameters("List").DefaultValue = line.ToString
+                Try
+                    SqlDataSource1.Insert()
+                Catch ex As Exception
+                    Me.lblEventName.Text = ex.ToString
+                    Exit Sub
+                End Try
+            End If
         Next
         Me.TextBox1.Text = ""
         Session.Clear()
